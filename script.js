020103cf854aa2c709648e743b7f340950c97aa8
Ali_Sdg90 <115698554+Ali-Sdg90@users.noreply.github.com>
2023-01-18 20:50:57 +0330

    Fix localSave / Fix completeBtn / Add function to filter btns / change html filter btns

--- script.js
+++ script.js
@@ -8,17 +8,47 @@ const localTodo = localStorage.getItem("saveTodos");
 const defaultTodo = "Add your new todo";
 let todoSaves = [];
 let editTodo = -1;
+let firstTopFilter = 35.5;
+let filterList = "all";
 todoInput.value = defaultTodo;
 // localStorage.clear();
 
+if (localTodo) {
+    todoSaves = JSON.parse(localStorage.getItem("saveTodos"));
+    addToHtml(false);
+}
+function filterTodoSavesFunc() {
+    let tempTodoSaves = [];
+    switch (filterList) {
+        case "active":
+            todoSaves.forEach(function (save) {
+                if (!save.isComplete) {
+                    tempTodoSaves.push(save);
+                }
+            });
+            break;
+        case "completed":
+            todoSaves.forEach(function (save) {
+                if (save.isComplete) {
+                    tempTodoSaves.push(save);
+                }
+            });
+            break;
+        case "all":
+            tempTodoSaves = todoSaves;
+            break;
+    }
+    return tempTodoSaves;
+}
 function addToHtml(addNewTodo) {
-    if (!todoSaves.length) todoList.innerHTML = "<br />";
+    let filteredTodoSaves = filterTodoSavesFunc();
+    if (!filteredTodoSaves.length) todoList.innerHTML = "<br />";
     else todoList.innerHTML = "";
-    for (let i = todoSaves.length; i > 0; ) {
+    for (let i = filteredTodoSaves.length; i > 0; ) {
         i--;
         todoList.innerHTML += `
         <span id="todo-number${i}">
-            <div id="work-number${i}">${todoSaves[i]}</div>
+            <div id="work-number${i}">${filteredTodoSaves[i].todo}</div>
             <div id="edit-number${i}">
             <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M24 11l2.414-2.414c.781-.781.781-2.047 0-2.828l-2.172-2.172c-.781-.781-2.047-.781-2.828 0L19 6 24 11zM17 8L5.26 19.74C7.886 19.427 6.03 21.933 7 23c.854.939 3.529-.732 3.26 1.74L22 13 17 8zM4.328 26.944l-.015-.007c-.605.214-1.527-.265-1.25-1.25l-.007-.015L4 23l3 3L4.328 26.944z"/></svg>
             </div>
@@ -31,7 +61,7 @@ function addToHtml(addNewTodo) {
         </span>
         `;
     }
-    for (let i = 0; i < todoSaves.length; i++) {
+    for (let i = 0; i < filteredTodoSaves.length; i++) {
         document
             .getElementById(`edit-number${i}`)
             .addEventListener("click", function () {
@@ -47,10 +77,15 @@ function addToHtml(addNewTodo) {
             .addEventListener("click", function () {
                 deleteBtn(i);
             });
+        if (filteredTodoSaves[i].isComplete) {
+            document
+                .getElementById(`work-number${i}`)
+                .classList.toggle("complete-todo");
+        }
     }
     if (addNewTodo) {
         const newTodo = document.getElementById(
-            `todo-number${todoSaves.length - 1}`
+            `todo-number${filteredTodoSaves.length - 1}`
         );
         newTodo.style.transform = "translate(0, -3.5vh)";
         newTodo.style.opacity = "0";
@@ -59,15 +94,20 @@ function addToHtml(addNewTodo) {
             newTodo.style.transform = "translate(0, 0)";
         }, 10);
     }
-    todoCounter.textContent = todoSaves.length;
-    if (todoSaves.length == 1) {
+    todoCounter.textContent = filteredTodoSaves.length;
+    if (filteredTodoSaves.length == 1) {
         pendingTask.textContent = "pending task";
     } else {
         pendingTask.textContent = "pending tasks";
     }
+    if (filteredTodoSaves.length < 8) {
+        let filterTop = firstTopFilter - filteredTodoSaves.length * 2.95 + "vh";
+        document.getElementById("filter-nav-background").style.top = filterTop;
+        document.getElementById("filter-nav-text").style.top = filterTop;
+    }
 }
 function editBtn(todoNumber) {
-    todoInput.value = todoSaves[todoNumber];
+    todoInput.value = todoSaves[todoNumber].todo;
     editTodo = todoNumber;
     addBtn.textContent = "✓";
     addBtn.style.fontSize = "4.6vh";
@@ -75,49 +115,43 @@ function editBtn(todoNumber) {
     todoInput.focus();
 }
 function completeBtn(todoNumber) {
-    document
-        .getElementById(`work-number${todoNumber}`)
-        .classList.toggle("complete-todo");
+    if (todoSaves[todoNumber].isComplete) {
+        todoSaves[todoNumber].isComplete = false;
+    } else {
+        todoSaves[todoNumber].isComplete = true;
+    }
+    localStorage.setItem("saveTodos", JSON.stringify(todoSaves));
+    addToHtml(false);
 }
 function deleteBtn(todoNumber) {
+    //work
     todoSaves.splice(todoNumber, 1);
-    localStorage.setItem("saveTodos", todoSaves);
+    localStorage.setItem("saveTodos", JSON.stringify(todoSaves));
     document.getElementById(`todo-number${todoNumber}`).style.opacity = 0;
     setTimeout(() => {
         addToHtml(false);
-        filterTopCalc()
     }, 250);
 }
-if (localTodo) {
-    todoSaves = localTodo.split(",");
-    addToHtml(false);
+function addTodoSaves(newTodo, newIsComplete) {
+    this.todo = newTodo;
+    this.isComplete = newIsComplete;
 }
-let firstTopFilter = 35.3;
 addBtn.addEventListener("click", function () {
     if (!todoInput.value || todoInput.value == defaultTodo) return;
     if (editTodo != -1) {
-        todoSaves[editTodo] = todoInput.value;
+        todoSaves[editTodo].todo = todoInput.value;
         addBtn.textContent = "+";
         editTodo = -1;
-        localStorage.setItem("saveTodos", todoSaves);
+        localStorage.setItem("saveTodos", JSON.stringify(todoSaves));
         addToHtml(false);
         todoInput.value = defaultTodo;
         return;
     }
-    todoSaves.push(todoInput.value);
+    todoSaves[todoSaves.length] = new addTodoSaves(todoInput.value, false);
     addToHtml(true);
     todoInput.value = defaultTodo;
-    localStorage.setItem("saveTodos", todoSaves);
-    filterTopCalc()
+    localStorage.setItem("saveTodos", JSON.stringify(todoSaves));
 });
-function filterTopCalc(){
-    if (todoSaves.length < 8) {
-        let filterTop = firstTopFilter - todoSaves.length * 2.95 + "vh";
-        console.log(firstTopFilter);
-        document.getElementById("filter-nav-background").style.top = filterTop;
-        document.getElementById("filter-nav-text").style.top = filterTop;
-    }
-}
 todoInput.addEventListener("focusin", function () {
     if (editTodo == -1) todoInput.value = "";
 });
@@ -126,14 +160,36 @@ todoInput.addEventListener("focusout", function () {
 });
 todoCounter.textContent = todoSaves.length;
 clearAll.addEventListener("dblclick", function () {
-    localStorage.setItem("saveTodos", "");
     todoSaves = [];
-    todoList.innerHTML = "<br />";
-    todoCounter.textContent = todoSaves.length;
-    pendingTask.textContent = "pending tasks";
+    localStorage.setItem("saveTodos", []);
     todoInput.value = defaultTodo;
-    filterTopCalc()
+    addToHtml(false);
 });
-document.getElementById("filter-all").addEventListener("click",function(){
-    console.log("hi")
-})
+const filterBackground = document.getElementsByClassName("filter-background");
+const filtertext = document.getElementsByClassName("filter-text");
+const filterDivBackground = document.getElementsByClassName(
+    "filter-div-background"
+);
+for (let i = 0; i < 3; i++) {
+    filtertext[i].addEventListener("click", function () {
+        for (let j = 0; j < 3; j++) {
+            filterBackground[j].style.fill = "rgb(198, 198, 198)";
+            filterBackground[j].style.position = "relative";
+            filterDivBackground[j].style.zIndex = "1";
+        }
+        filterBackground[i].style.fill = "white";
+        filterDivBackground[i].style.zIndex = "2";
+        switch (i) {
+            case 0:
+                filterList = "all";
+                break;
+            case 1:
+                filterList = "active";
+                break;
+            case 2:
+                filterList = "completed";
+                break;
+        }
+        addToHtml(false);
+    });
+}
